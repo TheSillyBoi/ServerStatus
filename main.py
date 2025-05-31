97372ca49fdde22d994c8de64099e941d673a96e
Adrian Tennies <adrian@tennies.us>
2025-05-31 00:34:55 -0500

    fix

---
+++ main.py
@@ -0,0 +1,30 @@
+# This example requires the 'message_content' intent.
+
+import discord
+import psutil
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+processes = list(p.name() for p in psutil.process_iter())# This is where you put the name of the server file/process
+count = processes.count("minecraft_server")
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!Status'):
+        print('Status command received')
+        #await message.channel.send('Hello!')
+        if count == 0:
+            await message.channel.send('server is not running')
+        else:
+            await message.channel.send('server is running')
+
+print('Starting the bot...')
+
+client.run('') #put your bot token here that you get from discord.com/developers/applications
